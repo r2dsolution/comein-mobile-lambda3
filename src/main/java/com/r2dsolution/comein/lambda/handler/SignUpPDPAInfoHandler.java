6068eb5abc2336509d7621c7af2aad3ed9536eab
Tawatchai Radom <tawatchai@r2dsolution.com>
2022-09-25 17:24:07 +0700

    update response cognito

--- src/main/java/com/r2dsolution/comein/lambda/handler/SignUpPDPAInfoHandler.java
+++ src/main/java/com/r2dsolution/comein/lambda/handler/SignUpPDPAInfoHandler.java
@@ -4,13 +4,13 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.r2dsolution.comein.lambda.model.CognitoRequest;
 
-public class SignUpPDPAInfoHandler implements RequestHandler<CognitoRequest,String>{
+public class SignUpPDPAInfoHandler implements RequestHandler<CognitoRequest,CognitoRequest>{
 
 	@Override
-	public String handleRequest(CognitoRequest request, Context context) {
+	public CognitoRequest handleRequest(CognitoRequest request, Context context) {
 		context.getLogger().log("SignUp PAPD........");
 		//context.getLogger().log("SignUp PAPD input: "+input);
-		return "{}";
+		return request;
 	}
 
 }
