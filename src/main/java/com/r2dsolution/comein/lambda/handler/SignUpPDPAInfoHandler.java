7d4ab5fb259d98d2db5ad19fcf3dfccc250efd46
Tawatchai Radom <tawatchai@r2dsolution.com>
2022-09-25 18:26:06 +0700

    update by event

--- src/main/java/com/r2dsolution/comein/lambda/handler/SignUpPDPAInfoHandler.java
+++ src/main/java/com/r2dsolution/comein/lambda/handler/SignUpPDPAInfoHandler.java
@@ -2,18 +2,20 @@ package com.r2dsolution.comein.lambda.handler;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.CognitoUserPoolPreSignUpEvent;
 import com.r2dsolution.comein.lambda.model.CognitoRequest;
 
-public class SignUpPDPAInfoHandler implements RequestHandler<CognitoRequest,CognitoRequest>{
+public class SignUpPDPAInfoHandler implements RequestHandler<CognitoUserPoolPreSignUpEvent.Request,CognitoUserPoolPreSignUpEvent.Response>{
 
 	@Override
-	public CognitoRequest handleRequest(CognitoRequest request, Context context) {
-		context.getLogger().log("SignUp PAPD........");
-		for (String key: request.getDatasetRecords().keySet()) {
-			context.getLogger().log("SignUp PAPD -> key: "+key);
-		}
-		//context.getLogger().log("SignUp PAPD input: "+input);
-		return request;
+	public CognitoUserPoolPreSignUpEvent.Response handleRequest(CognitoUserPoolPreSignUpEvent.Request request, Context context) {
+		context.getLogger().log("Start SignUp PAPD........by event");
+		CognitoUserPoolPreSignUpEvent.Response response = new CognitoUserPoolPreSignUpEvent.Response();
+		response.setAutoConfirmUser(false);
+		response.setAutoVerifyEmail(false);
+		response.setAutoVerifyPhone(false);
+		context.getLogger().log("Finish SignUp PAPD........by event");
+		return response;
 	}
 
 }
