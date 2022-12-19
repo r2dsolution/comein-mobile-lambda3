ecdb1dc2fc08d7a62171095cb0176310ad2f60f8
Tawatchai Radom <tawatchai@r2dsolution.com>
2022-12-20 01:37:36 +0700

    Load Booked TourTicket

---
+++ src/main/java/com/r2dsolution/comein/lambda/handler/LoadBookedTourTicketHandler.java
@@ -0,0 +1,34 @@
+package com.r2dsolution.comein.lambda.handler;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.r2dsolution.comein.entity.view.BookedTourTicketView;
+import com.r2dsolution.comein.entity.view.TicketView;
+import com.r2dsolution.comein.entity.view.TourTicketView;
+import com.r2dsolution.comein.lambda.model.GateWayRequest;
+import com.r2dsolution.comein.model.ComeInMapper;
+import com.r2dsolution.comein.model.TourTicket;
+import com.r2dsolution.comein.repository.BookedTourTicketViewRepository;
+import com.r2dsolution.comein.repository.BookingInfoRepository;
+import com.r2dsolution.comein.repository.TourTicketViewRepository;
+import com.r2dsolution.comein.util.DateUtils;
+
+public class LoadBookedTourTicketHandler extends BaseGateWayHandler<GateWayRequest>{
+
+	@Override
+	protected Map<String, Object> doHandlerRequest(GateWayRequest input, Map<String, Object> output, Context context)
+			throws Exception {
+		BookedTourTicketViewRepository repo = ctx.getBean(BookedTourTicketViewRepository.class);
+		
+		String code = (String) input.getQuery("code");
+		String ownerId = input.getProfile().getComein_id();
+		log("param booking-code: "+code);
+		BookedTourTicketView v = repo.findByCodeAndOwnerId(code, ownerId);
+		output.put("result", ComeInMapper.map(v));
+		return output;
+	}
+
+}
--- src/main/java/com/r2dsolution/comein/repository/BookedTourTicketViewRepository.java
+++ src/main/java/com/r2dsolution/comein/repository/BookedTourTicketViewRepository.java
@@ -12,7 +12,7 @@ import com.r2dsolution.comein.entity.view.TourTicketView;
 public interface BookedTourTicketViewRepository extends Repository<BookedTourTicketView,String>{
 	
 	
-	BookedTourTicketView findByCode(String code);
+	BookedTourTicketView findByCodeAndOwnerId(String code,String ownerId);
 	
 	@Query("select * from v_booked_ticket where owner_id = :p_owner and tour_date >= :p_date")
 	List<BookedTourTicketView> findActiveTourBooking(@Param("p_owner") String ownerId,@Param("p_date")java.sql.Date tourDate);
